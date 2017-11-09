9167964ca0f1621a28951fae70fd74e4c3dc07e5
--global <--global>
2017-11-09 18:49:13 -0500

    add controllers for households

--- chore-turn/app/controllers/households_controller.rb
+++ chore-turn/app/controllers/households_controller.rb
@@ -1,2 +1,21 @@
 class HouseholdsController < ApplicationController
+  def show
+    @household = Household.find(params[:id])
+  end
+
+  def new
+
+  end
+
+  def create
+    @household = Household.new(household_params)
+
+    @household.save
+    redirect_to @household
+  end
+
+  private
+  def household_params
+    params.require(:household).permit(:name, :home_address)
+  end
 end
