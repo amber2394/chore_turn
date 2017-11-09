1fa85371690ec39d2239d7286f131aac96da2c73
Amber Maillard <amber.maillard23@gmail.com>
2017-11-09 16:49:45 -0500

    Remove duration validation

--- chore-turn/app/models/chore.rb
+++ chore-turn/app/models/chore.rb
@@ -1,5 +1,5 @@
 class Chore < ApplicationRecord
-  validates :name, :duration, :date, presence: true
+  validates :name, :date, presence: true
 
   belongs_to :household
   has_many :assignment_chores
