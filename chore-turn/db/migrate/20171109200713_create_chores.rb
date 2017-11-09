1bcf5cacf0425f021f049adb2a10784f21d4c5da
Amber Maillard <amber.maillard23@gmail.com>
2017-11-09 16:48:33 -0500

    Took off null false for duration attribute

--- chore-turn/db/migrate/20171109200713_create_chores.rb
+++ chore-turn/db/migrate/20171109200713_create_chores.rb
@@ -2,7 +2,7 @@ class CreateChores < ActiveRecord::Migration[5.1]
   def change
     create_table :chores do |t|
       t.string :name, null: false
-      t.integer :duration, null: false
+      t.integer :duration
       t.boolean :status, default: false
       t.date :date, null: false
       t.references :household, foreign_key: true
