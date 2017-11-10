c68e850e64541d28059742d33b68752722c57831
Chris Camp <cpcamp2@gmail.com>
2017-11-10 10:49:48 -0500

    Update household.rb
    Single comma

--- chore-turn/app/models/household.rb
+++ chore-turn/app/models/household.rb
@@ -1,6 +1,6 @@
 class Household < ApplicationRecord
   validates :name, :address, :city, :state, :zip_code, presence: true
-  validates :zip_code, , numericality: { equals_to: 5 }
+  validates :zip_code, numericality: { equals_to: 5 }
 
   has_many :chores
 end
